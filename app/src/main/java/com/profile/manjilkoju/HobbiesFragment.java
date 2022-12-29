28ac7289e04641455352bf9a6c6686d11aec8414
manjil <manjilkoju09@gmail.com>
2022-12-29 08:59:33 +0545

    bottom nav complete

--- app/src/main/java/com/profile/manjilkoju/HobbiesFragment.java
+++ app/src/main/java/com/profile/manjilkoju/HobbiesFragment.java
@@ -2,31 +2,23 @@ package com.profile.manjilkoju;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class ContactMeFragment extends Fragment {
+public class HobbiesFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact_me, container, false);
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_hobbies, container, false);
     }
 }
--- app/src/main/java/com/profile/manjilkoju/MainActivity.java
+++ app/src/main/java/com/profile/manjilkoju/MainActivity.java
@@ -12,6 +12,9 @@ import com.google.android.material.navigation.NavigationBarView;
 public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
     BottomNavigationView bottomNavigationView;
     HomeFragment homeFragment = new HomeFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
+    MySkillsFragment mySkillsFragment = new MySkillsFragment();
+    HobbiesFragment hobbiesFragment = new HobbiesFragment();
     ContactMeFragment contactMeFragment = new ContactMeFragment();
 
     @Override
@@ -28,6 +31,15 @@ public class MainActivity extends AppCompatActivity implements NavigationBarView
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == R.id.home){
             getSupportFragmentManager().beginTransaction().replace(R.id.flContainer, homeFragment).commit();
+            return true;
+        }else if (item.getItemId() == R.id.profile){
+            getSupportFragmentManager().beginTransaction().replace(R.id.flContainer, profileFragment).commit();
+            return true;
+        }else if (item.getItemId() == R.id.skills){
+            getSupportFragmentManager().beginTransaction().replace(R.id.flContainer, mySkillsFragment).commit();
+            return true;
+        }else if (item.getItemId() == R.id.hobbies){
+            getSupportFragmentManager().beginTransaction().replace(R.id.flContainer, hobbiesFragment).commit();
             return true;
         }else if (item.getItemId() == R.id.contactMe){
             getSupportFragmentManager().beginTransaction().replace(R.id.flContainer, contactMeFragment).commit();
